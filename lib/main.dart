2186369445fcfb2772a73c61a9dc58e98d2f7d38
MOHAMMED7SALAH <bentitimohammedsalahstars7@gmail.com>
2022-05-16 18:53:17 +0200

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,24 @@
 import 'package:cacl_app/controller.dart';
 import 'package:flutter/material.dart';
 
-import 'package:device_preview/device_preview.dart';
 import 'package:get/get_state_manager/src/simple/get_state.dart';
 
 void main() => runApp(
-  DevicePreview(
-    // enabled: !kReleaseMode,
-    builder: (context) => MyApp(), // Wrap your app
-  ),
+  // DevicePreview(
+  //   // enabled: !kReleaseMode,
+  //   builder: (context) => MyApp(), // Wrap your app
+  // ),
+  MyApp(),
 );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Calculator App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const MyHomePage(title: 'Calculator'),
     );
@@ -55,74 +56,69 @@ const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 50),
-        child: Scaffold(
-            appBar: AppBar(
-              centerTitle: true,
-              title: Text(title),
-            ),
-            body: Column(
-              children: [
-            Container(
-              alignment: Alignment.centerRight,
-              padding: const EdgeInsets.symmetric(
-                vertical: 24.0,
-                horizontal: 12.0
-              ),
-              child: GetBuilder<CalculatorController>(
-                    init: CalculatorController(),
-                    builder: (controller) =>Text(controller.output, style: const TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                
-              )))
-              
-            ),
-            const Expanded(
-              child: Divider(color: Colors.amber),
-            ),
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text(title),
+        ),
+        body: Column(
+          children: [
+        Container(
+          alignment: Alignment.centerRight,
+          padding: const EdgeInsets.symmetric(
+            vertical: 24.0,
+            horizontal: 12.0
+          ),
+          child: GetBuilder<CalculatorController>(
+                init: CalculatorController(),
+                builder: (controller) =>Text(controller.output, style: const TextStyle(
+            fontSize: 48.0,
+            fontWeight: FontWeight.bold,
             
+          )))
+          
+        ),
+        const Expanded(
+          child: Divider(color: Colors.amber),
+        ),
+        
 
-            Column(children: [
-              Row(children: [
-                buildButton("7"),
-                buildButton("8"),
-                
-                buildButton("9"),
-                buildButton("/")
-              ]),
+        Column(children: [
+          Row(children: [
+            buildButton("7"),
+            buildButton("8"),
+            
+            buildButton("9"),
+            buildButton("/")
+          ]),
 
-              Row(children: [
-                buildButton("4"),
-                buildButton("5"),
-                buildButton("6"),
-                buildButton("X")
-              ]),
+          Row(children: [
+            buildButton("4"),
+            buildButton("5"),
+            buildButton("6"),
+            buildButton("X")
+          ]),
 
-              Row(children: [
-                buildButton("1"),
-                buildButton("2"),
-                buildButton("3"),
-                buildButton("-")
-              ]),
+          Row(children: [
+            buildButton("1"),
+            buildButton("2"),
+            buildButton("3"),
+            buildButton("-")
+          ]),
 
-              Row(children: [
-                buildButton("."),
-                buildButton("0"),
-                buildButton("00"),
-                buildButton("+")
-              ]),
+          Row(children: [
+            buildButton("."),
+            buildButton("0"),
+            buildButton("00"),
+            buildButton("+")
+          ]),
 
-              Row(children: [
-                buildButton("CLEAR"),
-                buildButton("="),
-              ])
-            ])
-              ],
-            )),
-      ),
-    );
+          Row(children: [
+            buildButton("CLEAR"),
+            buildButton("="),
+          ])
+        ])
+          ],
+        ));
   }
 }
